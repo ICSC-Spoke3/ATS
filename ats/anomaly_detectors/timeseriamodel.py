dfdfec116db2ea6dcd85d0b94c8b944a13bd7e2a
clarasaia <clara.saia.1997@gmail.com>
2025-12-05 19:38:50 +0100

    Add conversion from timeseries_df to timeseries and back conversion in the TimeseriaAnomalyDetector class

--- ats/anomaly_detectors/timeseriamodel.py
+++ ats/anomaly_detectors/timeseriamodel.py
@@ -1,5 +1,7 @@
-from base import AnomalyDetector
+from ats.utils import convert_timeseries_df_to_timeseries, convert_timeseries_to_timeseries_df
+from .base import AnomalyDetector
 from timeseria.datastructures import TimeSeries
+from timeseria.models.anomaly_detectors import PeriodicAverageAnomalyDetector
 import pandas as pd
 
 # Setup logging
@@ -9,20 +11,28 @@ logger = logging.getLogger(__name__)
 class TimeseriaAnomalyDetector(AnomalyDetector):  
     
     def __init__(self):
-        self.model = None
+        self.model = PeriodicAverageAnomalyDetector()
         pass
 
     def apply(self,data):
         if not isinstance(data,pd.DataFrame):
             raise NotImplementedError()
         timeseries_df = data
-        print(timeseries_df)
-        # timeseria format conversion
-        model = PeriodicAverageAnomalyDetector()
+
+        # Using timeseria to fit and apply the model
+        timeseries = convert_timeseries_df_to_timeseries(timeseries_df)
+        model = self.model
         model.fit(timeseries)
         timeseries = model.apply(timeseries)
-        # convert back
-        
+
+        # Convert back to DataFrame
+        timeseries_df = convert_timeseries_to_timeseries_df(timeseries)
+        for col in timeseries_df.columns:
+            anomaly_col = f"{col}_anomaly"
+            timeseries_df[anomaly_col] = timeseries_df[f"{col}_anomaly"]
+            
+        return timeseries_df
+    
     # Implementare fit e apply
     # Aggiungere value_1_anomaly value_2_anomaly anomaly 
     # anomaly = unione (?)
