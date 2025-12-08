50506d34db388bee4aec6584b5884a0fef29c954
clarasaia <clara.saia.1997@gmail.com>
2025-12-08 14:42:01 +0100

    Add fit fucntion for a generic TimeseriaAnomalyDetector and generalize apply. Apply also Fit for implementing but will be modified

--- ats/anomaly_detectors/basetimeseriamodel.py
+++ ats/anomaly_detectors/basetimeseriamodel.py
@@ -11,27 +11,45 @@ logger = logging.getLogger(__name__)
 class TimeseriaAnomalyDetector(AnomalyDetector):  
     
     def __init__(self):
-        self.model = PeriodicAverageAnomalyDetector()
-
-    def apply(self,data):
+        self.model = None
+    
+    def fit(self, data, *args, **kwargs):
         if not isinstance(data,pd.DataFrame):
-            raise NotImplementedError()
+            raise NotImplementedError('Not yet implemented for non DataFrame inputs')
+        timeseries_df = data
+
+        # Using timeseria to fit the model
+        timeseries = convert_timeseries_df_to_timeseries(timeseries_df)
+        model = self.model
+        model.fit(timeseries, *args, **kwargs)
+        self.model = model
+
+     def apply(self, data, *args, **kwargs):
+        if not isinstance(data,pd.DataFrame):
+            raise NotImplementedError('Not yet implemented for non DataFrame inputs')
         timeseries_df = data
 
         # Using timeseria to fit and apply the model
         timeseries = convert_timeseries_df_to_timeseries(timeseries_df)
         model = self.model
-        model.fit(timeseries)
-        timeseries = model.apply(timeseries)
+
+        #TODO: remove fit from apply at some point
+        model.fit(timeseries, *args, **kwargs)
+        timeseries = model.apply(timeseries, *args, **kwargs)
 
         # Convert back to DataFrame
         timeseries_df = convert_timeseries_to_timeseries_df(timeseries)
-        #for col in timeseries_df.columns:
-         #   anomaly_col = f"{col}_anomaly"
-          #  timeseries_df[anomaly_col] = timeseries_df[f"{col}_anomaly"]
             
         return timeseries_df
     
     # Implementare fit e apply
     # Aggiungere value_1_anomaly value_2_anomaly anomaly 
     # anomaly = unione (?)
+
+class PeriodicAverageAnomalyDetector(TimeseriaAnomalyDetector):
+    def __init__(self):
+        super().__init__()
+        self.model = PeriodicAverageAnomalyDetector()
+
+    def apply(self,data):
+        return super().apply(data)
