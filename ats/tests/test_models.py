08a6ee76418337491b2ba7832c85e52e3fe666ad
clarasaia <clara.saia.1997@gmail.com>
2025-12-08 17:06:02 +0100

    Change test timeseries_df, now it is generating using generate_timeseries_df from utils.py

--- ats/tests/test_models.py
+++ ats/tests/test_models.py
@@ -3,30 +3,28 @@ import pandas as pd
 import numpy as np
 
 from ..anomaly_detectors.basetimeseriamodel import TimeseriaAnomalyDetector, PeriodicAverageAnomalyDetector
+from ats.utils import generate_timeseries_df
 
 # Setup logging
 from .. import logger
 logger.setup()
 
 class TestModels(unittest.TestCase):
+    def setUp(self):
+        # Generate a sample timeseries DataFrame for testing
+        self.data = generate_timeseries_df(entries=200, variables=2, freq='H')
+
     def test_timeseria_anomaly_detector_not_implemented(self):
         detector = TimeseriaAnomalyDetector()
         with self.assertRaises(NotImplementedError):
-            detector.fit(data=None)
+            detector.fit(self.data)
         with self.assertRaises(NotImplementedError):
-            detector.apply(data=None)
+            detector.apply(self.data)
 
     def test_periodic_average_anomaly_detector(self):
         detector = PeriodicAverageAnomalyDetector()
-        # Create a simple DataFrame for testing
-        date_range = pd.date_range(start='2023-01-01', periods=100, freq='H')
-        data = pd.DataFrame({
-            'timestamp': date_range,
-            'value': np.random.rand(100)
-        }).set_index('timestamp')
+        detector.fit(self.data)
+        applied_series = detector.apply(self.data)
 
-        detector.fit(data)
-        serie_fitted = detector.apply(data)
-
-        self.assertIsInstance(serie_fitted, pd.DataFrame)
-        self.assertEqual(serie_fitted.shape, data.shape)
+        self.assertIsInstance(applied_series, pd.DataFrame)
+        self.assertIn('anomaly', applied_series.columns)
