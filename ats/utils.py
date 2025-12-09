502e3f5a76f04f389970535f6024d21089a8b6ee
clarasaia <clara.saia.1997@gmail.com>
2025-12-09 20:21:44 +0100

    Change ts in timestap in convert_timeseries_to_timeseries_df function in utils.py

--- ats/utils.py
+++ ats/utils.py
@@ -46,10 +46,10 @@ def convert_timeseries_to_timeseries_df(timeseries):
     timeseries_df = TimeSeries.to_df(timeseries)
 
     for datapoint in timeseries:
-        ts = datapoint.dt  # or datapoint.t
+        timestamp = datapoint.dt  # or datapoint.t
         for data_label, value in datapoint.data_indexes.items():
             if data_label.endswith('anomaly'):
-                timeseries_df.at[ts, data_label] = value
+                timeseries_df.at[timestamp, data_label] = value
 
     timeseries_df.index.name = 'timestamp'
     timeseries_df.index.freq = timeseries_df.index.inferred_freq
