1a736a38386f75385f4f3416c9b139718473feb4
clarasaia <clara.saia.1997@gmail.com>
2025-12-09 16:46:22 +0100

    Refactor model_class implementation add Linear Regression

--- ats/anomaly_detectors/basetimeseriamodel.py
+++ ats/anomaly_detectors/basetimeseriamodel.py
@@ -3,6 +3,7 @@ from .base import AnomalyDetector
 from timeseria.datastructures import TimeSeries
 from timeseria.models.anomaly_detectors import PeriodicAverageAnomalyDetector as TimeseriaPeriodicAverageAnomalyDetector
 from timeseria.models.anomaly_detectors import LSTMAnomalyDetector as TimeseriaLSTMAnomalyDetector  
+from timeseria.models.anomaly_detectors import LinearRegressionAnomalyDetector as TimeseriaLinearRegressionAnomalyDetector              
 import pandas as pd
 
 # Setup logging
@@ -14,15 +15,17 @@ class TimeseriaAnomalyDetector(AnomalyDetector):
     Base class for anomaly detectors wrapped from the timeseria library.
     """  
     
-    def __init__(self):
-        self.model = None
-    
+    model_class = None
+
+    def __init__(self, *args, **kwargs):
+        if self.model_class is None:
+            raise NotImplementedError('Subclasses must define a timeseria model')
+        self.model = self.model_class(*args, **kwargs)
+
     def fit(self, data, *args, **kwargs):
         """
         Fit the timeseria anomaly detector model.
         """      
-        if self.model is None:
-            raise NotImplementedError('Subclasses must define a timeseria model')
         if not isinstance(data,pd.DataFrame):
             raise NotImplementedError('Not yet implemented for non DataFrame inputs')
         timeseries_df = data
@@ -36,29 +39,25 @@ class TimeseriaAnomalyDetector(AnomalyDetector):
         """
         Apply the timeseria anomaly detector model.
         """     
-        if self.model is None:
-            raise NotImplementedError('Subclasses must define a timeseria model')
         if not isinstance(data,pd.DataFrame):
             raise NotImplementedError('Not yet implemented for non DataFrame inputs')
         timeseries_df = data
 
         # Using timeseria to fit and apply the model
         timeseries = convert_timeseries_df_to_timeseries(timeseries_df)
-        model = self.model
-
-        timeseries = model.apply(timeseries, *args, **kwargs)
+        timeseries = self.model.apply(timeseries, *args, **kwargs)
 
         # Convert back to DataFrame
         timeseries_df = convert_timeseries_to_timeseries_df(timeseries)
-            
+        timeseries_df['anomaly'] = (timeseries_df['anomaly'].astype(float) != 0).astype(int)
+
         return timeseries_df
 
 class PeriodicAverageAnomalyDetector(TimeseriaAnomalyDetector):
-    def __init__(self):
-        super().__init__()
-        self.model = TimeseriaPeriodicAverageAnomalyDetector()
+    model_class = TimeseriaPeriodicAverageAnomalyDetector
 
 class LSTMAnomalyDetector(TimeseriaAnomalyDetector):
-    def __init__(self):
-        super().__init__()
-        self.model = TimeseriaLSTMAnomalyDetector()
+    model_class = TimeseriaLSTMAnomalyDetector
+
+class LinearRegressionAnomalyDetector(TimeseriaAnomalyDetector):
+    model_class = TimeseriaLinearRegressionAnomalyDetector
--- ats/tests/test_models.py
+++ ats/tests/test_models.py
@@ -15,12 +15,9 @@ class TestModels(unittest.TestCase):
         self.data = generate_timeseries_df(entries=200, variables=2, freq='H')
 
     def test_timeseria_anomaly_detector_not_implemented(self):
-        detector = TimeseriaAnomalyDetector()
         with self.assertRaises(NotImplementedError):
-            detector.fit(self.data)
-        with self.assertRaises(NotImplementedError):
-            detector.apply(self.data)
-
+            detector = TimeseriaAnomalyDetector()
+            
     def test_periodic_average_anomaly_detector(self):
         detector = PeriodicAverageAnomalyDetector()
         detector.fit(self.data)
