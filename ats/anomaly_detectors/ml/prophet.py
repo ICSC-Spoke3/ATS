122e30abf06d1737c88ab36d2144bec618897b14
clarasaia <clara.saia.1997@gmail.com>
2025-12-22 12:36:12 +0100

    Add prophet wrapper from timeseria

---
+++ ats/anomaly_detectors/ml/prophet.py
@@ -0,0 +1,22 @@
+from timeseria.models.forecasters import ProphetForecaster
+from timeseria.models.anomaly_detectors import ModelBasedAnomalyDetector
+from ..base import TimeseriaAnomalyDetector
+
+# Prophet-based Anomaly Detector wrapper, accept only univariate data
+
+class _ProphetBaseDetector(ModelBasedAnomalyDetector):
+    model_class = ProphetForecaster
+
+class ProphetAnomalyDetector(TimeseriaAnomalyDetector):
+    model_class = _ProphetBaseDetector
+    
+    #TODO: define right capabilities
+    capabilities = {
+        'mode': 'semi-supervised',
+        'streaming': True,
+        'context': 'window',
+        'granularity': 'point',
+        'multivariate': False,
+        'scope': 'agnostic'
+    }
+    
